b39a5e19a172a96ec650e962b8b37ce525c272ee
emisenhe <emisenhe@purdue.edu>
2023-04-26 12:51:29 -0400

    New changes to pass in url String

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,17 @@ use std::{
     collections::HashMap,
     io::{BufRead, BufReader},
 };
+use std::fs::File;
 
 #[allow(dead_code)]
-fn calcscore(f: &String) -> Result<(), String> {
+fn calcscore(url: String) -> Result<(), String> {
     let mut net_scores = Vec::new();
 
-    let file = std::fs::File::open(f).map_err(|e| format!("{}", e))?;
+    let mut f = File::create("/src/url.txt").expect("Unable to create file");
+    f.write_all(url.as_bytes()).expect("Unable to write data to file");
+    let file_path = "/src/url.txt";
+
+    let file = std::fs::File::open(file_path).map_err(|e| format!("{}", e))?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
