020c9c66590068d576737b0c2c782232bf73af19
GabbyWhitisStellantis <gabbylonza@gmail.com>
2023-04-27 14:59:36 -0400

    testing test in workflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,9 +6,9 @@ fi
 GITHUB_TOKEN="${{ secrets.TEAM_GITHUB_TOKEN }}" ./run tests/url.txt > log/test.log 2>&1
 
 if [ $? -eq 0 ]; then
-  passed=$(cat log/test.log | grep -o "ok. [0-9]* passed;" | sed 's/ok. \([0-9]*\) passed;/\1/')
-  failed=$(cat log/test.log | grep -o "[0-9]* failed;" | sed 's/\([0-9]*\) failed;/\1/')
-  total=$(cat log/test.log | grep -o "running [0-9]* tests" | sed 's/running \([0-9]*\) tests/\1/')
+  passed=$(cat log/testing.log | grep -o "ok. [0-9]* passed;" | sed 's/ok. \([0-9]*\) passed;/\1/')
+  failed=$(cat log/testing.log | grep -o "[0-9]* failed;" | sed 's/\([0-9]*\) failed;/\1/')
+  total=$(cat log/testing.log | grep -o "running [0-9]* tests" | sed 's/running \([0-9]*\) tests/\1/')
   echo "Total Tests: $total"
   echo "Passed Tests: $passed"
   echo "Coverage Test Percentage: unknown"
